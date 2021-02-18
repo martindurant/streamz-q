fae10c1cb2cb0d29cff3b664de5c4c19b1c9d04b
Martin Durant <martin.durant@utoronto.ca>
2021-02-18 15:59:59 -0500

    streamer

--- hv_roi/roi.py
+++ hv_roi/roi.py
@@ -1,12 +1,16 @@
+import asyncio
 import holoviews as hv
 from holoviews.operation.datashader import rasterize
 import numpy as np
 import panel as pn
 from panel.pane.holoviews import HoloViews
+from streamz.sources import Source
+import threading
+import time
 hv.extension('bokeh')
 
 
-class from_hv_selection:
+class from_hv_selection(Source):
     def __init__(self, im, xcoords=None, ycoords=None, zcoords=None, scale="linear",
                  limits="percentile", vmin=5, vmax=95, colorbar=True,
                  cmap="gray", axes_names=None, select_type="box"):
@@ -15,7 +19,8 @@ class from_hv_selection:
         xcoords = xcoords or np.arange(nx)
         ycoords = ycoords or np.arange(ny)
         self.polys = hv.Polygons([]).opts(alpha=0.1)
-        cls = {"box": hv.streams.BoxEdit}
+        cls = {"box": hv.streams.BoxEdit,
+               "tap": hv.streams.Tap}
         self.stream = cls[select_type](source=self.polys)
         if more:
             zcoords = zcoords or np.arange(more[0])
@@ -28,26 +33,46 @@ class from_hv_selection:
         self.out = self.ds.to(hv.Image, axes_names[-2:]).opts(
             cmap=cmap, colorbar=colorbar,
             height=400, width=int(400 * len(xcoords) / len(ycoords))) #, cnorm=scale)
+        self.stream.param.watch(self.cb, 'x')
         done = pn.widgets.Button(name="done")
-        # self.stream.param.watch(self.cb, 'data')
-        done.param.watch(self.cb, 'clicks')
+        done.param.watch(self.stop, 'clicks')
         pl = HoloViews(self.out * self.polys)
         self.plot = pn.Column(pl, done)
+        self.data = []
+        super().__init__(asynchronous=True)
 
     def cb(self, *args):
-        if self.stream.data["x1"]:
-            self.loop.stop()
+        self.data.append((self.stream.x, self.stream.y))
+        self.loop.add_callback(self.emitter)
+
+    async def emitter(self):
+        out = self.emit(self.data.pop(0), asynchronous=True)
+        if not out.done():
+            await asyncio.gather(out)
 
     def mask(self):
         img = self.out.last if isinstance(self.out, hv.HoloMap) else self.out
         return rasterize(self.stream.element, target=img, aggregator='any',
                          dynamic=False)
 
-    def show(self):
-        server = pn.io.server.get_server(self.plot, start=False, show=True)
-        self.loop = server.io_loop.asyncio_loop
-        try:
-            self.loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        return #self.mask()
+    def stop(self, *args):
+        e.set()
+        super().stop()
+        self.loop.stop()
+
+    async def run(self):
+        self.server = pn.io.server.get_server(self.plot, start=False, show=True,
+                                              loop=self.loop)
+
+
+if __name__ == "__main__":
+    e = threading.Event()
+    data = np.random.random((10, 10))
+    s = from_hv_selection(data, select_type="tap")
+    s = s.buffer(20).rate_limit(interval=1)
+    s.sink(print)
+    s.start()
+    try:
+        e.wait()
+    except KeyboardInterrupt:
+        pass
